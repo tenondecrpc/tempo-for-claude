104e0b89931328e89caf9fd6cbf117063c02c4e9
Cristian Paniagua <tenondecrpc@gmail.com>
2026-03-27 10:08:51 -0300

    feat: Add new watch app files

---
+++ ClaudeTracker Watch Extension/Claude_Tracker_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_Tracker_WatchApp.swift
+//  Claude Tracker Watch Watch App
+//
+//  Created by Cristian Paniagua on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Claude_Tracker_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeTracker Watch/Claude_Tracker_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_Tracker_WatchApp.swift
+//  Claude Tracker Watch
+//
+//  Created by Cristian Paniagua on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Claude_Tracker_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
