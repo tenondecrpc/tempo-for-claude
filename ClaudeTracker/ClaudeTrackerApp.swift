33cc5725a498ef1b42964ac2539825433a6e4e83
Cristian Paniagua <tenondecrpc@gmail.com>
2026-03-27 09:18:33 -0300

    Initial Commit

---
+++ ClaudeTracker/ClaudeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTrackerApp.swift
+//  ClaudeTracker
+//
+//  Created by Cristian Paniagua on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
